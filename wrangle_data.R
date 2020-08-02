2b506be23b27d558e1853abd31d31bd21d528b1a
Christopher Page <cpagefam5@aol.com>
2020-08-02 12:54:36 -0400

    Committing wrangle_data.R, a script written to read "commodity_prices_2010_2020.csv" into a data framed named "commodities" and save that data frame as a compressed .RDATA file.

--- wrangle_data.R
+++ wrangle_data.R
@@ -1,5 +1,4 @@
-### Generate a data frame from commodity_prices_2010_2020.csv
-### Save that data frame as commodities.rdata
-library(tidyverse)
+### First, this script reads "data/commodity_prices_2010_2020.csv" into a new data frame named" commodities"
+### Then, it saves that data frame into a compressed .RDATA file under the RDA subdirectory
 commodities <- read_csv("data/commodity_prices_2010_2020.csv")
 save(commodities, file = "rda/commodities.rdata")
