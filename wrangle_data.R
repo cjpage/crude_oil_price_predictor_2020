19edc935d0f09c834e8d3c0ddc7de308c33e493a
Christopher Page <65598163+cjpage@users.noreply.github.com>
2020-08-02 07:58:17 -0400

    Add files via upload
    Committing wrangle_data.R, an R file written to convert "commodity_prices_2020.csv" into the "commodities" data frame and save that data frame as a compressed .RDATA file under the RDA subdirectory.

---
+++ wrangle_data.R
@@ -0,0 +1,8 @@
+<<<<<<< HEAD
+library(tidyverse)
+commodities <- read_csv("data/commodity_prices_2010_2020.csv")
+save(commodities, file = "rda/commodities.rdata")
+=======
+library(tidyverse)
+commodities <- read_csv("data/commodity_prices_2010_2020.csv")
+>>>>>>> 744e7e96aa3baf9c5a2d82ddf9f5417a5c887a94
