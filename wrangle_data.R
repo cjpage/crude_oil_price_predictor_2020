9f1cd7fd5cf6ad6acb81d27147c756b1ffc7608e
Christopher Page <cpagefam5@aol.com>
2020-08-02 12:54:36 -0400

    Commit update to wrangle_data.R.  This update mutates the "Date" column of commodity_prices_2020.2020.csv to a vector of variables with a class of Date after recognzing the mdy format of the original information.

--- wrangle_data.R
+++ wrangle_data.R
@@ -1,5 +1,7 @@
-### Generate a data frame from commodity_prices_2010_2020.csv
-### Save that data frame as commodities.rdata
-library(tidyverse)
+### First, this script reads "data/commodity_prices_2010_2020.csv" into a new data frame named" commodities"
+### Then, it saves that data frame into a compressed .RDATA file under the RDA subdirectory
+library(lubridate)
 commodities <- read_csv("data/commodity_prices_2010_2020.csv")
 save(commodities, file = "rda/commodities.rdata")
+commodities <- commodities %>%
+  mutate(Date = as.Date(mdy(Date)))
