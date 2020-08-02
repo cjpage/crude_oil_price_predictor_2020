7a6dfead0a65a6c9db37f521ffd7bbdfdc17977b
Christopher Page <cpagefam5@aol.com>
2020-08-02 12:47:11 -0400

    Deleting wrangle_data.R

--- wrangle_data.R
+++ wrangle_data.R
@@ -2,4 +2,4 @@
 ### Save that data frame as commodities.rdata
 library(tidyverse)
 commodities <- read_csv("data/commodity_prices_2010_2020.csv")
-save(c0mmodities, file = "rda/commodities.rdata")
+save(commodities, file = "rda/commodities.rdata")
