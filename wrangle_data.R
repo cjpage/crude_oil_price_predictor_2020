023b06e0ceb3c1ba98d2cd773c8acc2b2e638964
Christopher Page <65598163+cjpage@users.noreply.github.com>
2020-08-02 12:08:59 -0400

    Updating wrangle_data.R, the script used to read the "commodity_prices_2010_2020" CSV file, place the results in the "commodities" data frame under the DATA subdirectory, and save the data frame as a .RDATA compressed file under the RDA subdirectory.

--- wrangle_data.R
+++ wrangle_data.R
@@ -2,5 +2,4 @@
 ### Save that data frame as commodities.rdata
 library(tidyverse)
 commodities <- read_csv("data/commodity_prices_2010_2020.csv")
-save(commodities, file = "rda/commodities.rdata")
-
+save(c0mmodities, file = "rda/commodities.rdata")
