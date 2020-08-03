8b7dcd1410df60d5275353d55614e5abe5ee55c9
Christopher Page <65598163+cjpage@users.noreply.github.com>
2020-08-03 06:02:01 -0400

    Delete wrangle_data.R

--- wrangle_data.R
+++
@@ -1,4 +0,0 @@
-### First, this script reads "data/commodity_prices_2010_2020.csv" into a new data frame named" commodities"
-### Then, it saves that data frame into a compressed .RDATA file under the RDA subdirectory
-commodities <- read_csv("data/commodity_prices_2010_2020.csv")
-save(commodities, file = "rda/commodities.rdata")
