be64d47a979fe633960abe46f4fe691b10034763
Christopher Page <cpagefam5@aol.com>
2020-08-04 04:42:01 -0400

    Another minor update of the crude oil history plot

--- analysis_of_crude_oil_price_history.R
+++ analysis_of_crude_oil_price_history.R
@@ -1,16 +1,18 @@
 library(tidyverse)
 library(ggplot2)
+library(ggthemes)
 load("rda/commodities.rdata")
 
 ### First, this script plots the 2010-2020 timeline of crude oil daily closing prices
 ### Then, it saves the plot in a .png file entitled "crude_oil_price_history"
 
-commodities %>%
+crude_oil_plot <- commodities %>%
   ggplot(aes(date, crude_oil_net_change_since_start_date)) +
   geom_point(color = "red") +
   xlab("Date") +
   ylab("Net Change of Closing Price") +
-  ggtitle("Crude Oil Price History August 2010 to July 2020")
+  ggtitle("Crude Oil Price History: 8/2010 - 7/2020")
+  theme_economist()
 
 ggsave("crude_oil_price_history.png")
 
