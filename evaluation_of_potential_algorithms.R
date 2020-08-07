31c735d31e01971635d96b7eacb798cd27d828be
Christopher Page <cpagefam5@aol.com>
2020-08-06 22:08:10 -0400

    Update of script used to evalue potential algorithms

--- evaluation_of_potential_algorithms.R
+++ evaluation_of_potential_algorithms.R
@@ -1,4 +1,5 @@
 ### This script evaluates a series of potentially viable algorithms for predicting cruide oil prices
+### Each of these four algorithms considers crude oil in isolation from other commodities
 
 ### Algorithm 01: Predicted Crude Oil Price Based on Average Crude Oil Price
 
@@ -16,11 +17,12 @@ RMSE01
 
 crude_oil_average_by_day_of_the_week <- crude_oil_train %>%
   group_by(date_weekday) %>%
-  summarize(b_d = mean(closing_price - mu))
+  summarize(b_w = mean(closing_price))
 
-predicted_price_algorithm_02 <- mu + crude_oil_test %>%
+predicted_price_algorithm_02 <- crude_oil_test %>%
   left_join(crude_oil_average_by_day_of_the_week, by= 'date_weekday') %>%
-  pull(b_d)
+  mutate(pred = b_w) %>%
+  pull(pred)
 
 RMSE02 <- RMSE(predicted_price_algorithm_02, crude_oil_test$closing_price)
 
@@ -30,15 +32,16 @@ RMSE02
 
 crude_oil_average_by_month_of_the_year <- crude_oil_train %>%
   group_by(date_month) %>%
-  summarize(b_m = mean(closing_price - mu))
+  summarize(b_m = mean(closing_price))
 
-predicted_price_algorithm_03 <- mu + crude_oil_test %>%
+predicted_price_algorithm_03 <- crude_oil_test %>%
   left_join(crude_oil_average_by_month_of_the_year, by= 'date_month') %>%
-  pull(b_m)
+  mutate(pred = b_m) %>%
+  pull(pred)
 
-RMSE04 <- RMSE(predicted_price_algorithm_03, crude_oil_test$closing_price)
+RMSE03 <- RMSE(predicted_price_algorithm_03, crude_oil_test$closing_price)
 
-RMSE04
+RMSE03
 
 ### Algorithm 04: Predicted Crude Oil Price Based on Average Crude Oil Price plus Quarter of the Year Effects
 
@@ -46,35 +49,26 @@ crude_oil_average_by_quarter_of_the_year <- crude_oil_train %>%
   group_by(date_quarter) %>%
   summarize(b_q = mean(closing_price - mu))
 
-predicted_price_algorithm_03 <- mu + crude_oil_test %>%
+predicted_price_algorithm_04 <- mu + crude_oil_test %>%
   left_join(crude_oil_average_by_quarter_of_the_year, by= 'date_quarter') %>%
   pull(b_q)
 
-RMSE04 <- RMSE(predicted_price_algorithm_03, crude_oil_test$closing_price)
+RMSE04 <- RMSE(predicted_price_algorithm_04, crude_oil_test$closing_price)
 
 RMSE04
 
-### Algorithm 05: Predicted Crude Oil Price Based on Average Crude Oil Price plus Day of Week and Month and Quarter of the Year Effects
+### Algorithm 05: Predicted Crude Oil Price Based on Average Crude Oil Price plus Year Effects
 
-crude_oil_average_by_day_of_the_week <- crude_oil_train %>%
-  group_by(date_weekday) %>%
-  summarize(b_d = mean(closing_price - mu))
-
-crude_oil_average_by_month_of_the_year <- crude_oil_train %>%
-  group_by(date_month) %>%
-  summarize(b_m = mean(closing_price - mu))
-
-crude_oil_average_by_quarter_of_the_year <- crude_oil_train %>%
-  group_by(date_quarter) %>%
-  summarize(b_q = mean(closing_price - mu))
+crude_oil_average_by_year <- crude_oil_train %>%
+  group_by(date_year) %>%
+  summarize(b_y = mean(closing_price - mu))
 
 predicted_price_algorithm_05 <- mu + crude_oil_test %>%
-  left_join(crude_oil_average_by_day_of_the_week, by= 'date_weekday') %>%
-  left_join(crude_oil_average_by_month_of_the_year, by= 'date_month') %>%
-  left_join(crude_oil_average_by_quarter_of_the_year, by= 'date_quarter') %>%
-  mutate(pred = mu + b_d + b_m + b_q) 
+  left_join(crude_oil_average_by_year, by= 'date_year') %>%
+  pull(b_y)
 
 RMSE05 <- RMSE(predicted_price_algorithm_05, crude_oil_test$closing_price)
 
 RMSE05
+
 
