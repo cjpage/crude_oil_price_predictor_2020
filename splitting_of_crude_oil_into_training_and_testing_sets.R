3577d9d188b4e20b30d078f7f2c80ce73f0998e4
Christopher Page <cpagefam5@aol.com>
2020-08-08 06:42:12 -0400

    Updating wrangle_data.R

--- splitting_of_crude_oil_into_training_and_testing_sets.R
+++ splitting_of_crude_oil_into_training_and_testing_sets.R
@@ -1,15 +1,5 @@
 ### This script splits "crude_oil" into training and testing data sets
 
-library(tidyverse)
-library(ggplot2)
-library(ggthemes)
-library(ggrepel)
-library(dplyr)
-library(lubridate)
-library(caret)
-
-load("rda/commodities.rdata")
-
 set.seed(755)
 
 test_index <- createDataPartition(y = crude_oil$commodity, times = 1, p = 0.2, list = FALSE)
--- wrangle_data.R
+++ wrangle_data.R
@@ -27,7 +27,13 @@ commodities <- commodities %>%
 
 save(commodities, file = "rda/commodities.rdata")
 
-### This part generates and saves data sets focused on crude oil and the energy, precious metals, and agricultural sectors
+### This part generates and saves a data set focused on crude oil
+
+crude_oil <- energy %>%
+  filter(commodity == 'Crude Oil')
+save(crude_oil, file = "rda/crude_oil.rdata")
+
+### This part generates and saves data sets focused on the energy, precious metals, and agricultural sectors
 
 energy <- commodities %>%
   filter(sector == 'Energy')
@@ -41,26 +47,55 @@ agriculture <- commodities %>%
   filter(sector == 'Agriculture')
 save(agriculture, file = "rda/agriculture.rdata")
 
-### This part generates and saves data sets focused on each of the individual commodities in the three sectors
-
-crude_oil <- energy %>%
-  filter(commodity == 'Crude Oil') %>%
-  mutate(crude_oil_closing_price = closing_price)
-save(crude_oil, file = "rda/crude_oil.rdata")
+### This part generates and saves data sets focused on the commodities (other than crude oil) in the energy sector
 
 natural_gas <- energy %>%
   filter(commodity == 'Natural Gas') %>%
-  mutate(natural_gas_closing_price = closing_price)
+  rename(natural_gas_closing_price = closing_price)
 save(natural_gas, file = "rda/natural_gas.rdata")
 
 heating_oil <- energy %>%
   filter(commodity == 'Heating Oil') %>%
-  mutate(heating_oil_closing_price = closing_price)
+  rename(heating_oil_closing_price = closing_price)
 save(natural_gas, file = "rda/heating_oil.rdata")
 
 gasoline <- energy %>%
   filter(commodity == 'Gasoline') %>%
-  mutate(gasoline_closing_price = closing_price)
+  rename(gasoline_closing_price = closing_price)
 save(gasoline, file = "rda/gasoline.rdata")
+
+### This part generates and saves data sets focused on the commodities in the precious metals sector
+
+gold <- precious_metals %>%
+  filter(commodity == 'Gold') %>%
+  rename(gold_closing_price = closing_price)
+save(natural_gas, file = "rda/gold.rdata")
+
+silver <- precious_metals %>%
+  filter(commodity == 'Silver') %>%
+  rename(silver_closing_price = closing_price)
+save(natural_gas, file = "rda/silver.rdata")
+
+platinum <- precious_metals %>%
+  filter(commodity == 'Platinum') %>%
+  rename(platinum_closing_price = closing_price)
+save(gasoline, file = "rda/platinum.rdata")
+
+### This part generates and saves data sets focused on the commodities in the agriculture sector
+
+wheat <- agriculture %>%
+  filter(commodity == 'Wheat') %>%
+  rename(wheat_closing_price = closing_price)
+save(wheat, file = "rda/wheat.rdata")
+
+rice <- agriculture %>%
+  filter(commodity == 'Rice') %>%
+  rename(rice_closing_price = closing_price)
+save(rice, file = "rda/rice.rdata")
+
+soybeans <- agriculture %>%
+  filter(commodity == 'Soybeads') %>%
+  rename(soybeans_closing_price = closing_price)
+save(soybeans, file = "rda/soybeans.rdata")
 
 
